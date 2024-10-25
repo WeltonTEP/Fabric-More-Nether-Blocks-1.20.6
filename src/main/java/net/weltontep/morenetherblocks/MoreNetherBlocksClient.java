9fb195abb07772e6acb8fb48bb0e931eefe6f3fa
WeltonTEP <WeltonTEP@gmail.com>
2024-10-25 14:54:31 -0700

    Initial commit

---
+++ src/main/java/net/weltontep/morenetherblocks/MoreNetherBlocksClient.java
@@ -0,0 +1,10 @@
+package net.weltontep.morenetherblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreNetherBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/weltontep/morenetherblocks/MoreNetherBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.weltontep.morenetherblocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreNetherBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
